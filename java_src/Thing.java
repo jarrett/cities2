0b0577e03b9465f120aabac1a988165fdab53239
Jarrett Colby <jarrettcolby@gmail.com>
2012-11-11 21:58:51 -0600

    Working on Thing class

--- java_src/Thing.java
+++ java_src/Thing.java
@@ -2,20 +2,40 @@ package cities;
 
 import java.util.zip.*;
 import java.util.ArrayList;
+import java.util.Map;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import org.yaml.snakeyaml.Yaml;
 
 class Thing {
   String path;
   String name;
   static ArrayList<String> usedNames = new ArrayList<String>();
   
+  private void loadFiles() throws IOException {
+    Yaml yaml = new Yaml();
+    Map spriteConfig;
+    if (path.endsWith(".zip")) {
+      ZipFile zip = new ZipFile(path);
+      spriteConfig = (Map) yaml.load(
+        zip.getEntry(name + "_spring_config.yml").toString()
+      );
+    } else {
+      spriteConfig = (Map) yaml.load(
+        new FileInputStream(new File(path + "/" + name + "_sprite_config.yml"))
+      );
+    }
+  }
+  
   // Path to its data files
   public Thing(String name, String path) throws IOException {
     this.path = path;
     this.name = name;
-    if (path.endsWith(".zip")) {
-      ZipFile zip = new ZipFile(path);
-      zip.getEntry("");
+    if (usedNames.indexOf(name) != 1) {
+      throw new RuntimeException("Conflicting names: You have at least two Things named " + name);
     }
+    usedNames.add(name);
+    loadFiles();
   }
 }
